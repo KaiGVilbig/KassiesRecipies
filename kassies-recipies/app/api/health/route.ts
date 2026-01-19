dee6740c10d92a1bb37f4fd1afa339bbd1b17457
Kai Vilbig <kaikundes18@gmail.com>
2026-01-19 15:22:28 +0000

    health check

--- kassies-recipies/app/api/health/route.ts
+++ kassies-recipies/app/api/health/route.ts
@@ -1,10 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return await getRecipies();
-}
-
-// GET: Get
-async function getRecipies() {
-    return NextResponse.json({status: 200})
+    return NextResponse.json({ status: "ok" });
 }
